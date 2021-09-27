46c363b1452a73b44f15915891430f1bdc4faaa5
rrll3553 <liveoak35@gmail.com>
2021-09-27 13:32:03 -0700

    Created functioning table view. Will render a list of all the movie titles.

--- FlixsterScroll/MoviesViewController.swift
+++ FlixsterScroll/MoviesViewController.swift
@@ -7,10 +7,20 @@
 
 import UIKit
 
-class MoviesViewController: UIViewController {
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    var movies = [[String:Any]]() // creates an array of dictionaries
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        
         print("Hello")
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
@@ -21,6 +31,9 @@ class MoviesViewController: UIViewController {
                     print(error.localizedDescription)
              } else if let data = data {
                     let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                
+                    self.movies = dataDictionary["results"] as! [[String:Any]]
+                self.tableView.reloadData()
                     print(dataDictionary)
 
                     // TODO: Get the array of movies
@@ -33,6 +46,22 @@ class MoviesViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+            
+        let cell = UITableViewCell()
+        let movie = movies[indexPath.row]
+        let title = movie["title"] as! String // casting it as a string
+        
+        cell.textLabel!.text = title
+        
+        return cell
+    }
 
     /*
     // MARK: - Navigation
